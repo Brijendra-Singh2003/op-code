cccab96e9335f04c16d257f840d399be0556ba39
Brijendra-Singh2003 <brijendra0369@gmail.com>
2026-05-05 19:42:35 +0530

    add debouncing for free tier

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,6 @@
+import asyncio
 import os
+from asyncio import sleep
 
 from dotenv import load_dotenv
 from google import genai
@@ -25,53 +27,72 @@ tools = types.Tool(
 config = types.GenerateContentConfig(tools=[tools])
 history: list[types.Content] = []
 
-while True:
-    s = input("> ")
-    content = types.Content(role="user", parts=[types.Part(text=s)])
-    history.append(content)
 
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=history, config=config
-    )
-
-    while response.function_calls:
-        candidate = response.candidates[0] if response.candidates else None
-        if candidate and candidate.content:
-            history.append(candidate.content)
-
-        for function_call in response.function_calls:
-            print(f"Function to call: {function_call.name}")
-            print(f"ID: {function_call.id}")
-            print(f"Arguments: {function_call.args}")
-
-            args = function_call.args or {}
-            result = None
-            match function_call.name:
-                case FileListTool.list_files_and_directories.name:
-                    result = FileListTool.list_files_and_directories_impl(**args)
-                case FileReadTool.read_file.name:
-                    result = FileReadTool.read_file_impl(**args)
-                case FileEditTool.apply_patch.name:
-                    result = FileEditTool.apply_patch_impl(**args)
-                case FileWriteTool.create_n_write_file.name:
-                    result = FileWriteTool.create_n_write_file_impl(**args)
-                case _:
-                    result = None
-
-            print(f"Result: {result}")
-
-            function_response_part = types.Part.from_function_response(
-                name=function_call.name or "",
-                response={"result": result},
+async def get_response():
+    sleep_duration = 8
+    while True:
+        try:
+            response = client.models.generate_content(
+                model="gemini-3-flash-preview", contents=history, config=config
             )
 
-            history.append(types.Content(role="user", parts=[function_response_part]))
+            return response
+        except Exception as e:
+            print(f"error: {str(e)}")
+            sleep_duration *= 2
+            await sleep(sleep_duration)
 
-        response = client.models.generate_content(
-            model="gemini-3-flash-preview",
-            config=config,
-            contents=history,
+
+async def main():
+    while True:
+        s = input("> ")
+        content = types.Content(role="user", parts=[types.Part(text=s)])
+        history.append(content)
+
+        response = await get_response()
+
+        while response.function_calls:
+            candidate = response.candidates[0] if response.candidates else None
+            if candidate and candidate.content:
+                history.append(candidate.content)
+
+            for function_call in response.function_calls:
+                print(f"Function to call: {function_call.name}")
+                print(f"ID: {function_call.id}")
+                print(f"Arguments: {function_call.args}")
+
+                args = function_call.args or {}
+                result = None
+                match function_call.name:
+                    case FileListTool.list_files_and_directories.name:
+                        result = FileListTool.list_files_and_directories_impl(**args)
+                    case FileReadTool.read_file.name:
+                        result = FileReadTool.read_file_impl(**args)
+                    case FileEditTool.apply_patch.name:
+                        result = FileEditTool.apply_patch_impl(**args)
+                    case FileWriteTool.create_n_write_file.name:
+                        result = FileWriteTool.create_n_write_file_impl(**args)
+                    case _:
+                        result = None
+
+                print(f"Result: {result}")
+
+                function_response_part = types.Part.from_function_response(
+                    name=function_call.name or "",
+                    response={"result": result},
+                )
+
+                history.append(
+                    types.Content(role="user", parts=[function_response_part])
+                )
+
+            response = await get_response()
+
+        history.append(
+            types.Content(role="model", parts=[types.Part(text=response.text)])
         )
+        print(response.text)
 
-    history.append(types.Content(role="model", parts=[types.Part(text=response.text)]))
-    print(response.text)
+
+if __name__ == "__main__":
+    asyncio.run(main())
